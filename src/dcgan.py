7c09a4d76f99f68ec67f0f48186c073be2b1bbfc
Adam <astevens9234@outlook.com>
2024-10-15 10:25:32 -0700

    added dcgan discriminator block

--- src/dcgan.py
+++ src/dcgan.py
@@ -39,3 +39,23 @@ class G_block(nn.Module):
 
 class D_block(nn.Module):
     """Discriminator Block."""
+
+    def __init__(
+        self,
+        out_channels,
+        in_channels=3,
+        kernel_size=4,
+        stride=2,
+        padding=1,
+        alpha=0.2,
+        **kwargs
+    ):
+        super(D_block, self).__init__(**kwargs)
+        self.conv2d = nn.Conv2d(
+            in_channels, out_channels, kernel_size, stride, padding, bias=False
+        )
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+        self.activation = nn.LeakyReLU(alpha, inplace=True)
+
+    def forward(X, self):
+        return self.activation(self.batch_norm(self.conv2d(X)))
